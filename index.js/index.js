d646e5280431df6579e375a7406ebb0879424d37
Seonghoon Oh <su5785@naver.com>
2021-08-20 01:17:05 +0900

    meet almost requirements
    code style & CSS work needed

--- index.js/index.js
+++ index.js/index.js
@@ -6,24 +6,30 @@ const $restAnswerCount = document.querySelector('.status-bar__rest-answer-count'
 const $gameTimer = document.querySelector('.status-bar__timer');
 const $resultContainer = document.querySelector('.result');
 const $resultImage = document.querySelector('.result__img');
+const $restartButton = document.querySelector('.result__restart');
+
+const $backgroundMusic = document.querySelector('.audio-background-music');
+const $pickSound = document.querySelector('.audio-pick');
+const $correctSound = document.querySelector('.audio-correct');
+const $gameFailSound = document.querySelector('.audio-game-fail');
+const $gameSuccessSound = document.querySelector('.audio-game-success');
 
 const CLASSNAME_NO_SHOW = 'no-show';
 const CLASSNAME_NO_POINTER_EVENT = 'no-pointer-event';
 
-const DEFAULT_SOURCE = './images/default.png';
+const DEFAULT_IMG_SOURCE = './images/default.png';
 const FAIL_IMG_SOURCE = './images/default.png'; //
 const SUCCESS_IMG_SOURCE = './images/success.png';
 
-
 const TOTAL_GOAL_POINT = 8;
-let restAnswerCount = TOTAL_GOAL_POINT;
-let correctPoint = 0;
+let restAnswerCount = null;
+let correctPoint = null;
 
 const TIME_LIMIT = 50;
-let timeLimit = TIME_LIMIT;
+let timeLimit = null;
 let timerId = null;
 
-const generateNumberPool = {
+let generateNumberPool = {
   0: 0,
   1: 1,
   2: 2,
@@ -46,6 +52,65 @@ const imagePool = [ 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8 ];
 const shuffledOrderList = [];
 const shuffledSourceList = [];
 
+$backgroundMusic.volume = 0.2;
+$pickSound.volume = 0.2;
+$correctSound.volume = 0.2;
+$gameFailSound.volume = 0.2;
+$gameSuccessSound.volume = 0.2;
+
+
+
+function resetForStart () {
+  restAnswerCount = TOTAL_GOAL_POINT;
+  $restAnswerCount.textContent = `남은 뚱이 : ${restAnswerCount}`;
+  correctPoint = 0;
+
+  timeLimit = TIME_LIMIT;
+  $gameTimer.textContent = `남은 시간 : ${timeLimit}`;
+  timerId = null;
+
+  generateNumberPool = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    11: 11,
+    12: 12,
+    13: 13,
+    14: 14,
+    15: 15,
+  };
+
+  shuffledOrderList.length = 0;
+  shuffledSourceList.length = 0;
+
+////DOM 요소로 안잡혀서 classList undefind 라고 계속 나오는건가?
+  $gameBoard.childNodes.forEach((element) => {
+    element.src = DEFAULT_IMG_SOURCE;
+    //if (!element.classList.includes(CLASSNAME_NO_POINTER_EVENT)) return;
+
+    //element.classList.remove(CLASSNAME_NO_POINTER_EVENT);
+  });
+  console.log($gameBoard.children)
+  console.dir($gameBoard.children)
+////
+
+  const cellList = Array.from($gameBoard.children);
+  cellList.forEach((element) => {
+    if (!element.classList) return;
+
+    element.classList.remove(CLASSNAME_NO_POINTER_EVENT);
+  })
+}
+
+console.dir($backgroundMusic)
 
 function startTimer() {
   timeLimit = TIME_LIMIT;
@@ -53,7 +118,7 @@ function startTimer() {
   timerId = setInterval(() => {
     $gameTimer.textContent = `남은 시간 : ${--timeLimit}`;
 
-    //시간초과시 1.타이머정지[o] 2. 실패 보여주기[o] 3. 실패 효과음 [ ]
+    //시간초과시 1.타이머정지[o] 2. 실패 보여주기[o] 3. 실패 효과음 [o]
     if (timeLimit === 0) {
       clearInterval(timerId);
       showEnding(false);
@@ -86,11 +151,15 @@ function shuffleImagesOrder() {
 }
 
 
+
 $gameStartContainer.addEventListener('click', handleGameStartButtonClick = (event) => {
   event.currentTarget.classList.add(CLASSNAME_NO_SHOW);
   $gameBoard.classList.remove(CLASSNAME_NO_SHOW);
   $statusContainer.classList.remove(CLASSNAME_NO_SHOW);
 
+  
+  $backgroundMusic.play();
+  resetForStart();
   startTimer();
   shuffleImagesOrder();
 });
@@ -104,9 +173,12 @@ $gameBoard.addEventListener('click', handleCellClick);
 
 function handleCellClick(event) {
   if (event.target === event.currentTarget) { return; }
+  $pickSound.play();
+  $pickSound.currentTime = 0;
 
   const $clicked = event.target;
   $clicked.src = shuffledSourceList[$clicked.dataset.index];
+  $clicked.classList.add(CLASSNAME_NO_POINTER_EVENT);
 
   toCompareElementList.push($clicked)
   toCompareImageSourceList.push($clicked.src);
@@ -126,6 +198,9 @@ function checkTheAnswer(elementList) {
   if (!(toCompareImageSourceList[0] === toCompareImageSourceList[1])) {
     backToDefaultAndReset();
     console.log('오답 !')
+    elementList.forEach((element) => {
+      element.classList.remove(CLASSNAME_NO_POINTER_EVENT);
+    });
   } else if (toCompareImageSourceList[0] === toCompareImageSourceList[1]) {
     console.log(toCompareImageSourceList)
     console.log('정답!!!!!!!!!')
@@ -134,14 +209,12 @@ function checkTheAnswer(elementList) {
     //데이터리셋해주기 [o]
     //다시 클릭못하게 만들기 no-pointer-event [o]
     //더블클릭 방지  [ ]
-    //정답 효과음 [ ]
-    elementList.forEach((element) => {
-      element.classList.add(CLASSNAME_NO_POINTER_EVENT);
-    });
-
+    //정답 효과음 [o]
+  
     increaseCorrectPoint();
     resetCheckingTools();
-
+    $correctSound.play();
+    $correctSound.currentTime = 0;
   }
 }
 
@@ -149,13 +222,13 @@ function checkTheAnswer(elementList) {
 function backToDefaultAndReset() {
   setTimeout(function() {
     toCompareElementList.forEach((element) => {
-      element.src = DEFAULT_SOURCE;
+      element.src = DEFAULT_IMG_SOURCE;
     });
     console.log("TIMEOUT 실행");
 
     //Q. 분리하면 setTimeout콜백함수 실행전에 되버림..
     resetCheckingTools();
-  }, 300);
+  }, 200);
 }
 
 function resetCheckingTools() {
@@ -179,7 +252,25 @@ function increaseCorrectPoint() {
 function showEnding(boolean) {
   $resultImage.src = boolean ? SUCCESS_IMG_SOURCE : FAIL_IMG_SOURCE;
 
+  boolean ? $gameSuccessSound.play() : $gameFailSound.play();
+
+  $backgroundMusic.pause();
+  $backgroundMusic.load();   //음악 리셋
+
   $gameBoard.classList.add(CLASSNAME_NO_SHOW);
   $statusContainer.classList.add(CLASSNAME_NO_SHOW);
   $resultContainer.classList.remove(CLASSNAME_NO_SHOW);
 }
+
+$resultContainer.addEventListener('click', (event) => {
+  event.currentTarget.classList.add(CLASSNAME_NO_SHOW);
+  $gameBoard.classList.remove(CLASSNAME_NO_SHOW);
+  $statusContainer.classList.remove(CLASSNAME_NO_SHOW);
+
+  $backgroundMusic.play();
+
+  resetForStart();
+  startTimer();
+  shuffleImagesOrder();
+});
+
